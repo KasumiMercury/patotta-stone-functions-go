6f041d96f8e6829e81557552ef3c21c6972c1017
KasumiMercury <naoki_shira@outlook.jp>
2024-03-25 22:49:00 +0900

    :construction: Get video records from supabase / to get upcoming video's info

--- animus/pkg/model/video.go
+++ animus/pkg/model/video.go
@@ -1,6 +1,20 @@
 package model
 
+import (
+	"github.com/uptrace/bun"
+	"time"
+)
+
 type VideoInfo struct {
 	SourceID string `json:"sourceId"`
 	ChatID   string `json:"chatId"`
+}
+
+type VideoRecord struct {
+	bun.BaseModel `bun:"table:videos"`
+
+	SourceID  string    `bun:",type:varchar(255)"`
+	Status    string    `bun:",type:varchar(255)"`
+	ChatID    string    `bun:",type:varchar(255)"`
+	UpdatedAt time.Time `bun:",type:timestamp"`
 }
