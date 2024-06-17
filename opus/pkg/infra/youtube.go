22d4689358d8b76f6b169cf2ecf97a95fb022339
KasumiMercury <naoki_shira@outlook.jp>
2024-06-17 21:29:17 +0900

    :construction: fetch schedule at from YouTube data api

--- opus/pkg/infra/youtube.go
+++ opus/pkg/infra/youtube.go
@@ -30,7 +30,7 @@ func (r YouTubeRepository) FetchVideoDetailsByVideoIDs(ctx context.Context, vide
 	return resp.Items, nil
 }
 
-func (r YouTubeRepository) FetchScheduleAtByVideoIDs(ctx context.Context, videoIDs []string) ([]*youtube.Video, error) {
+func (r YouTubeRepository) FetchScheduledAtByVideoIDs(ctx context.Context, videoIDs []string) ([]*youtube.Video, error) {
 	call := r.ytSvc.Videos.List([]string{"snippet"}).Id(videoIDs...)
 	call = call.Context(ctx)
 
--- opus/pkg/repository/youtube.go
+++ opus/pkg/repository/youtube.go
@@ -7,4 +7,5 @@ import (
 
 type YouTube interface {
 	FetchVideoDetailsByVideoIDs(ctx context.Context, videoIDs []string) ([]*youtube.Video, error)
+	FetchScheduledAtByVideoIDs(ctx context.Context, videoIDs []string) ([]*youtube.Video, error)
 }
