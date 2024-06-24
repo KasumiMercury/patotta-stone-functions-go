900bad08dc33c6596008db0654dcf83e2e7c26ab
KasumiMercury <naoki_shira@outlook.jp>
2024-06-24 22:32:26 +0900

    :construction: define realtime client

---
+++ opus/internal/adapter/output/realtime/client.go
@@ -0,0 +1,19 @@
+package realtime
+
+import (
+	"database/sql"
+	"github.com/uptrace/bun"
+	"github.com/uptrace/bun/dialect/pgdialect"
+	"github.com/uptrace/bun/driver/pgdriver"
+)
+
+type Realtime struct {
+	db *bun.DB
+}
+
+func NewRealtimeClient(dsn string) (*Realtime, error) {
+	sqldb := sql.OpenDB(pgdriver.NewConnector(pgdriver.WithDSN(dsn)))
+	db := bun.NewDB(sqldb, pgdialect.New())
+
+	return &Realtime{db: db}, nil
+}
