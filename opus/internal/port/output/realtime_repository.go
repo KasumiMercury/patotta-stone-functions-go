c3bda2c2ce5bd96e1b0580cbec28e0574edc18e1
KasumiMercury <naoki_shira@outlook.jp>
2024-06-24 22:45:30 +0900

    :construction: define realtime repository

---
+++ opus/internal/port/output/realtime_repository.go
@@ -0,0 +1,11 @@
+package output
+
+import (
+	"context"
+	"github.com/KasumiMercury/patotta-stone-functions-go/opus/internal/core/domain/realtime"
+)
+
+type RealtimeRepository interface {
+	GetRecordsBySourceIDs(ctx context.Context, sourceIDs []string) ([]*realtime.Record, error)
+	InsertRecords(ctx context.Context, records []*realtime.Record) error
+}
