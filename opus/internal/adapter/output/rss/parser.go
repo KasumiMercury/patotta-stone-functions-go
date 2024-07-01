cf9bc1c23373c188433701b323d43a371cd61f19
KasumiMercury <naoki_shira@outlook.jp>
2024-07-01 22:49:05 +0900

    :construction: generate mock

--- opus/internal/adapter/output/rss/parser.go
+++ opus/internal/adapter/output/rss/parser.go
@@ -1,9 +1,15 @@
 package rss
 
+//go:generate mockgen -source=$GOFILE -destination=./mocks/mock_$GOFILE -package=mocks
+
 import (
 	"context"
 	"github.com/mmcdole/gofeed"
 )
+
+type ParserRepository interface {
+	ParseURLWithContext(url string, ctx context.Context) (*gofeed.Feed, error)
+}
 
 type Parser struct {
 }
