7e7257660f579f9c7da81fcd348b2278c42b0eb9
KasumiMercury <naoki_shira@outlook.jp>
2024-07-02 00:38:37 +0900

    :construction: parser repository

---
+++ opus/internal/port/output/parser_repository.go
@@ -0,0 +1,12 @@
+package output
+
+//go:generate mockgen -source=$GOFILE -destination=./mocks/mock_$GOFILE -package=mocks
+
+import (
+	"context"
+	"github.com/mmcdole/gofeed"
+)
+
+type ParserRepository interface {
+	ParseURLWithContext(url string, ctx context.Context) (*gofeed.Feed, error)
+}
