2e768f385fc2cce89737f999aa41765f0e016171
KasumiMercury <naoki_shira@outlook.jp>
2024-07-04 22:00:08 +0900

    :construction: fetch video details

--- opus/internal/core/service/rss_service.go
+++ opus/internal/core/service/rss_service.go
@@ -3,11 +3,13 @@ package service
 import (
 	"context"
 	"github.com/KasumiMercury/patotta-stone-functions-go/opus/internal/port/output"
+	"log/slog"
 )
 
 type RssService struct {
 	rtdRepo output.RealtimeRepository
 	rssRepo output.RSSRepository
+	apiRepo output.ApiRepository
 }
 
 func NewRssService(rtd *output.RealtimeRepository) *RssService {
@@ -19,7 +21,7 @@ func NewRssService(rtd *output.RealtimeRepository) *RssService {
 func (r *RssService) UpdateVideosFromRssItem(ctx context.Context) error {
 	// Get the latest update time of the video info in the database(RealtimeDB)
 	// To eliminate RSS that has already been confirmed
-	lutu, err := r.rtdRepo.GetLastUpdatedUnixOfVideo(ctx)
+	luu, err := r.rtdRepo.GetLastUpdatedUnixOfVideo(ctx)
 	if err != nil {
 		return err
 	}
@@ -27,20 +29,32 @@ func (r *RssService) UpdateVideosFromRssItem(ctx context.Context) error {
 	duri := "https://www.youtube.com/feeds/videos.xml?channel_id=UCeLzT-7b2PBcunJplmWtoDg"
 
 	// Get updated videos from RSS
-	rss, err := r.rssRepo.FetchRssItems(ctx, duri, lutu)
+	rss, err := r.rssRepo.FetchRssItems(ctx, duri, luu)
 	if err != nil {
 		return err
 	}
 
 	// Extract source IDs from updated rss
-	sourceIDs := make([]string, 0, len(rss))
+	sidList := make([]string, 0, len(rss))
 	for _, r := range rss {
-		sourceIDs = append(sourceIDs, r.SourceID())
+		sidList = append(sidList, r.SourceID())
 	}
 
-	// TODO: Implement
-	// Get video info of updated videos from YouTube Data API
-	// TODO: Implement
+	// Get video details of updated videos from YouTube Data API
+	vdList, err := r.apiRepo.FetchVideoDetailsByVideoIDs(ctx, sidList)
+	if err != nil {
+		return err
+	}
+
+	// if difference between len(rss) and len(vdList) is not 0, log it as a warning
+	if len(rss) != len(vdList) {
+		slog.Warn(
+			"Failed to get video details for all updated videos",
+			"rss", len(rss),
+			"videoDetails", len(vdList),
+		)
+	}
+
 	// Update video info
 	// TODO: Implement
 
