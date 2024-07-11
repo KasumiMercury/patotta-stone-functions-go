d909aaa3c56d4d42b00a659c6b37d2a8039db7f8
KasumiMercury <naoki_shira@outlook.jp>
2024-07-11 20:03:31 +0900

    feat: add repository of upsert method

--- opus/internal/port/output/realtime_repository.go
+++ opus/internal/port/output/realtime_repository.go
@@ -3,10 +3,12 @@ package output
 import (
 	"context"
 	"github.com/KasumiMercury/patotta-stone-functions-go/opus/internal/adapter/output/db/realtime"
+	"github.com/KasumiMercury/patotta-stone-functions-go/opus/internal/core/domain/video"
 	"time"
 )
 
 type RealtimeRepository interface {
+	UpsertRecords(ctx context.Context, videos []video.Video) error
 	GetRecordsBySourceIDs(ctx context.Context, sourceIDs []string) ([]*realtime.Record, error)
 	InsertRecords(ctx context.Context, records []realtime.Record) error
 	UpdateRecords(ctx context.Context, records []realtime.Record) error
