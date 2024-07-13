5667f7418e9785c53f5c2591f654be6b99954517
KasumiMercury <naoki_shira@outlook.jp>
2024-07-13 18:22:53 +0900

    fix: accept id list

--- opus/internal/adapter/output/api/client.go
+++ opus/internal/adapter/output/api/client.go
@@ -13,8 +13,8 @@ func NewYouTubeImpl(svc *youtube.Service) *Client {
 	return &Client{svc: svc}
 }
 
-func (y *Client) VideoList(ctx context.Context, part []string, id string) (*youtube.VideoListResponse, error) {
-	call := y.svc.Videos.List(part).Id(id)
+func (y *Client) VideoList(ctx context.Context, part []string, id []string) (*youtube.VideoListResponse, error) {
+	call := y.svc.Videos.List(part).Id(id...)
 	call = call.Context(ctx)
 
 	return call.Do()
