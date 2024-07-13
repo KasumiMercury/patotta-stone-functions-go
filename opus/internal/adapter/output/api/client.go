d62b1a49fbb6556b52f0dc3f444c38d56adca61f
KasumiMercury <naoki_shira@outlook.jp>
2024-07-13 18:20:29 +0900

    refactor: rename

--- opus/internal/adapter/output/api/client.go
+++ opus/internal/adapter/output/api/client.go
@@ -5,15 +5,15 @@ import (
 	"google.golang.org/api/youtube/v3"
 )
 
-type YouTubeImpl struct {
+type Client struct {
 	svc *youtube.Service
 }
 
-func NewYouTubeImpl(svc *youtube.Service) *YouTubeImpl {
-	return &YouTubeImpl{svc: svc}
+func NewYouTubeImpl(svc *youtube.Service) *Client {
+	return &Client{svc: svc}
 }
 
-func (y *YouTubeImpl) VideoList(ctx context.Context, part []string, id string) (*youtube.VideoListResponse, error) {
+func (y *Client) VideoList(ctx context.Context, part []string, id string) (*youtube.VideoListResponse, error) {
 	call := y.svc.Videos.List(part).Id(id)
 	call = call.Context(ctx)
 
--- opus/internal/port/output/youtube_client_repository.go
+++ opus/internal/port/output/youtube_client_repository.go
@@ -5,6 +5,6 @@ import (
 	"google.golang.org/api/youtube/v3"
 )
 
-type YouTubeImpl interface {
+type Client interface {
 	VideoList(ctx context.Context, part []string, id string) (*youtube.VideoListResponse, error)
 }
