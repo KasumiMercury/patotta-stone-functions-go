ac54f906311ec5d1f10c1b375c700f3b3292911e
KasumiMercury <naoki_shira@outlook.jp>
2024-07-15 17:22:45 +0900

    feat: load setting

---
+++ opus/pkg/config/config.go
@@ -0,0 +1,43 @@
+package config
+
+import (
+	_ "embed"
+	"encoding/json"
+)
+
+//go:embed target.json
+var target string
+
+type Config struct {
+	target Target
+}
+
+type Target struct {
+	Channel []Channel `json:"channel"`
+}
+
+type Channel struct {
+	Display   string `json:"display"`
+	ChannelId string `json:"channelId"`
+}
+
+func NewConfig() (*Config, error) {
+	c := &Config{}
+	if err := c.loadTarget(target); err != nil {
+		return nil, err
+	}
+
+	return c, nil
+}
+
+func (c *Config) loadTarget(j string) error {
+	// load target channels from target.json
+	t := &Target{}
+	if err := json.Unmarshal([]byte(j), t); err != nil {
+		return err
+	}
+
+	c.target = *t
+
+	return nil
+}
