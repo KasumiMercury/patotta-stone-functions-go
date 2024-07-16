21e3fee504968b68f124e570bdcda7d9c8e57586
KasumiMercury <naoki_shira@outlook.jp>
2024-07-16 23:10:53 +0900

    feat: initializer

--- opus/internal/core/domain/api/api_model.go
+++ opus/internal/core/domain/api/api_model.go
@@ -15,11 +15,18 @@ type VideoDetail struct {
 	scheduledAtUnix int64
 }
 
-func NewVideoDetail(sourceID string) *VideoDetail {
-	return &VideoDetail{
-		sourceID: sourceID,
-		status:   status.Undefined,
+func NewVideoDetail(sourceID string, chatID string, status status.Status, pubAtUnix int64, scdAtUnix int64) (*VideoDetail, error) {
+	if pubAtUnix == 0 {
+		return nil, fmt.Errorf("publishedAtUnix is not set for sourceID: %s", sourceID)
 	}
+
+	return &VideoDetail{
+		sourceID:        sourceID,
+		chatID:          chatID,
+		status:          status,
+		publishedAtUnix: pubAtUnix,
+		scheduledAtUnix: scdAtUnix,
+	}, nil
 }
 
 func (vd *VideoDetail) SetChatID(chatID string) {
