bc0e1b0f26eadd89f8cb4539f5eca6045349cb35
KasumiMercury <naoki_shira@outlook.jp>
2024-07-23 15:14:17 +0900

    feat: create YouTube client

--- opus/internal/adapter/output/api/client.go
+++ opus/internal/adapter/output/api/client.go
@@ -2,6 +2,8 @@ package api
 
 import (
 	"context"
+	"github.com/KasumiMercury/patotta-stone-functions-go/opus/pkg/config"
+	"google.golang.org/api/option"
 	"google.golang.org/api/youtube/v3"
 )
 
@@ -9,8 +11,13 @@ type Client struct {
 	svc *youtube.Service
 }
 
-func NewYouTubeClient(svc *youtube.Service) *Client {
-	return &Client{svc: svc}
+func NewYouTubeClient(c config.Config) (*Client, error) {
+	svc, err := youtube.NewService(context.Background(), option.WithAPIKey(c.Api.ApiKey))
+	if err != nil {
+		return nil, err
+	}
+
+	return &Client{svc: svc}, nil
 }
 
 func (y *Client) VideoList(ctx context.Context, part []string, id []string) (*youtube.VideoListResponse, error) {
