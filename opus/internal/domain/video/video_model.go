cc3b5a413e79a895e03bd097b1bdff139c27629d
KasumiMercury <naoki_shira@outlook.jp>
2024-07-26 18:04:03 +0900

    refactor: validate

--- opus/internal/domain/video/video_model.go
+++ opus/internal/domain/video/video_model.go
@@ -1,6 +1,7 @@
 package video
 
 import (
+	"fmt"
 	"github.com/KasumiMercury/patotta-stone-functions-go/opus/status"
 )
 
@@ -16,8 +17,8 @@ type Video struct {
 	updatedAtUnix   int64
 }
 
-func NewVideo(channelID, sourceID, title, description, chatID string, status status.Status, publishedAtUnix, scheduledAtUnix, updatedAtUnix int64) *Video {
-	return &Video{
+func NewVideo(channelID, sourceID, title, description, chatID string, status status.Status, publishedAtUnix, scheduledAtUnix, updatedAtUnix int64) (*Video, error) {
+	v := &Video{
 		channelID:       channelID,
 		sourceID:        sourceID,
 		title:           title,
@@ -28,4 +29,45 @@ func NewVideo(channelID, sourceID, title, description, chatID string, status sta
 		scheduledAtUnix: scheduledAtUnix,
 		updatedAtUnix:   updatedAtUnix,
 	}
+
+	if err := v.validate(); err != nil {
+		return nil, err
+	}
+
+	return v, nil
+}
+
+func (v *Video) validate() error {
+	if v.channelID == "" {
+		return fmt.Errorf("channelID is required")
+	}
+	if v.sourceID == "" {
+		return fmt.Errorf("sourceID is required")
+	}
+	if v.title == "" {
+		return fmt.Errorf("title is required")
+	}
+
+	// description is optional
+	// chatID is optional
+
+	if v.status == status.Undefined {
+		return fmt.Errorf("status is undefined")
+	}
+
+	if v.publishedAtUnix == 0 {
+		return fmt.Errorf("publishedAtUnix is required")
+	}
+
+	// scheduledAtUnix is optional
+	// scheduledAtUnix is must be greater than publishedAtUnix
+	if v.scheduledAtUnix != 0 && v.scheduledAtUnix <= v.publishedAtUnix {
+		return fmt.Errorf("scheduledAtUnix must be greater than publishedAtUnix")
+	}
+
+	if v.updatedAtUnix == 0 {
+		return fmt.Errorf("updatedAtUnix is required")
+	}
+
+	return nil
 }
