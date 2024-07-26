10a91dd5b606e544121b4eb703cdcf2e496075e6
KasumiMercury <naoki_shira@outlook.jp>
2024-07-26 19:09:00 +0900

    test: fix parallel

--- opus/internal/domain/video/video_model_test.go
+++ opus/internal/domain/video/video_model_test.go
@@ -376,9 +376,9 @@ func TestVideo_NillableScheduledAt(t *testing.T) {
 	}
 
 	for _, tt := range tests {
-		t.Parallel()
 		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
 			got := tt.v.NillableScheduledAtUTC()
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("NillableScheduledAtUTC() got = %v, want %v", got, tt.want)
