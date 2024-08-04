91e113404b6701e68a20ea75ff3c4a4f1262a562
KasumiMercury <naoki_shira@outlook.jp>
2024-08-04 23:11:47 +0900

    fix: fix unique setting

--- opus/internal/adapters/db/realtime/realtime_model.go
+++ opus/internal/adapters/db/realtime/realtime_model.go
@@ -11,7 +11,7 @@ import (
 type Record struct {
 	bun.BaseModel `bun:"table:videos"`
 
-	SourceID    string     `bun:",type:varchar(255)"`
+	SourceID    string     `bun:",type:varchar(255),unique"`
 	Title       string     `bun:",type:varchar(255)"`
 	Status      string     `bun:",type:varchar(255)"`
 	ChatID      string     `bun:",type:varchar(255)"`
