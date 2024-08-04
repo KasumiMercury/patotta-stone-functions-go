646479999593d1825490fff8874216d720de0d99
KasumiMercury <naoki_shira@outlook.jp>
2024-08-04 22:47:50 +0900

    test: fix testcontainer

--- opus/test/testcontainers/realtime_container.go
+++ opus/test/testcontainers/realtime_container.go
@@ -2,38 +2,32 @@ package testcontainers
 
 import (
 	"context"
-	"github.com/docker/go-connections/nat"
 	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/modules/postgres"
 	"github.com/testcontainers/testcontainers-go/wait"
 	"time"
 )
 
-type realtimeContainer struct {
-	testcontainers.Container
+type RealtimeContainer struct {
+	*postgres.PostgresContainer
 }
 
-func SetUpRealtimeContainer(ctx context.Context) (*realtimeContainer, error) {
-	port, _ := nat.NewPort("tcp", "8080")
+func SetUpRealtimeContainer(ctx context.Context) (*RealtimeContainer, error) {
 	timeout := 2 * time.Minute
-	req := testcontainers.ContainerRequest{
-		Image:        "postgres:13",
-		ExposedPorts: []string{port.Port()},
-		Env: map[string]string{
-			"POSTGRES_USER":     "postgres",
-			"POSTGRES_PASSWORD": "password",
-			"POSTGRES_DB":       "test",
-		},
-		WaitingFor: wait.ForListeningPort(port).WithStartupTimeout(timeout),
-	}
 
-	container, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
-		ContainerRequest: req,
-		Started:          true,
-	})
+	container, err := postgres.Run(ctx,
+		"docker.io/postgres:16-alpine",
+		postgres.WithUsername("postgres"),
+		postgres.WithPassword("password"),
+		postgres.WithDatabase("test"),
+		testcontainers.WithWaitStrategy(
+			wait.ForLog("database system is ready to accept connections").WithOccurrence(2).WithStartupTimeout(timeout),
+		),
+	)
 
 	if err != nil {
 		return nil, err
 	}
 
-	return &realtimeContainer{container}, nil
+	return &RealtimeContainer{container}, nil
 }
